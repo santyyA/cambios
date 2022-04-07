16f8ba4f7b26a613d80db644ae765740460952da
santyyA <santy17apablaza@gmail.com>
2022-04-07 11:18:00 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { ServicioComponent } from './pages/servicio/servicio.component';
+import { EquipoComponent } from './pages/equipo/equipo.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'equipo', component: EquipoComponent},
+  {path:'servicio', component: ServicioComponent},
+  {path:'contacto', component: ContactoComponent},
+  {path:'**', component:NotPageFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './menu/header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { EquipoComponent } from './pages/equipo/equipo.component';
+import { ServicioComponent } from './pages/servicio/servicio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactoComponent,
+    EquipoComponent,
+    ServicioComponent,
+    AboutComponent,
+    NotPageFoundComponent
   ],
   imports: [
     BrowserModule,
